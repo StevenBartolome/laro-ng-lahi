f44bb7ad7c454080d2ed2ed9bf41d8ff5056d7c0
StevenBartolome <marksteven.0211@gmail.com>
2025-12-13 07:48:10 +0800

    fix label

--- login.php
+++ login.php
@@ -53,7 +53,7 @@ if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
         
         <form id="loginForm">
             <div class="form-group">
-                <label for="email">Username</label>
+                <label for="email">Email</label>
                 <input type="text" id="email" required>
             </div>
             
